daed3f0fdcfb6b28991aca637497ba79de9f485f
Gonzalo <gonzalo.fce@gmail.com>
2021-03-25 18:02:21 +0100

    set for 1 type of life-cycle (then we can copy) adapt to generic period of projection edit matrix for real vs complex numbers

--- 02_Caswell_Assignment.R
+++ 02_Caswell_Assignment.R
@@ -20,40 +20,29 @@ View(pen2)
 # B) one breeding adult penguin
 # C) a population with the stable stage distribution
 
-
-###### Extracting eigenvectors/values from A ###### 
+###### Normal Population: penguinmat1.txt ######
+####### Extracting eigenvectors/values from A ####### 
 eigen.penguin1 <- eigen(pen1)
 
-###### Let's find the one that checks Perron-Frobenius ###### 
+####### Let's find the one that checks Perron-Frobenius ####### 
 lambda1.position.pen1 <- which.max(abs(eigen.penguin1$values))
 
-###### Eigenvalue + Rigth Eigenvector ###### 
+####### Eigenvalue + Rigth Eigenvector ####### 
 lambda1.pen1 <- eigen.penguin1$values[lambda1.position.pen1]
 omega1.pen1 <- eigen.penguin1$vectors[,lambda1.position.pen1]
 
-###### LeftEigenvector ###### 
+####### LeftEigenvector ####### 
 ve1 <- eigen(t(pen1))$vectors[,lambda1.position.pen1]
 
 
-###### U matrix ###### 
-pen1[1,7] <- 0
+####### U matrix ####### 
 U.pen1 <- pen1
-
-pen2[1,7] <- 0
-U.pen2 <- pen2
-
-###### Importing data ###### 
-pen1 <- as.matrix(read.table("penguinmat1.txt"))
-pen2 <- as.matrix(read.table("penguinmat2.txt"))
+U.pen1[1,7] <- 0
 
 
-###### Calculating Fundamental Matrix N for both penguin Universes ###### 
+####### Calculating Fundamental Matrix N for both penguin Universes ####### 
 
 N.pen1 <- solve(diag(dim(U.pen1)[1]) - U.pen1)
-
-N.pen2 <- solve(diag(dim(U.pen2)[1]) - U.pen2)
-
-
 
 
 #### 3. Population projection ####
@@ -70,14 +59,20 @@ c1.b <- as.vector(ve1 %*% n0.b)
 c1.c <- as.vector(ve1 %*% n0.c)
 
 
+##### Looping for X periods #####
+periods <- 100
+n.a <- matrix(,nrow=dim(U.pen1)[1],ncol=periods+1)
+n.b <- matrix(,nrow=dim(U.pen1)[1],ncol=periods+1)
+n.c <- matrix(,nrow=dim(U.pen1)[1],ncol=periods+1)
 
-##### Looping for 10 periods #####
-n.a <- matrix(,nrow=7,ncol=11)
 
-for (t in 0:10) {
-  n.a[,t+1] <- c1.a %*% (lambda1^t) %*% omega1
+for (t in 0:periods) {
+  n.a[,t+1] <- as.numeric(c1.a %*% (lambda1.pen1^t) %*% omega1.pen1)
+  n.b[,t+1] <- as.numeric(c1.b %*% (lambda1.pen1^t) %*% omega1.pen1)
+  n.c[,t+1] <- as.numeric(c1.c %*% (lambda1.pen1^t) %*% omega1.pen1)
 }
 
-n.a <- as.data.frame(n.a)
-names(n.complete) <- c("n0", "n1", "n2", "n3", "n4", "n5",
-                       "n6", "n7", "n8", "n9", "n10")
+
+
+
+
